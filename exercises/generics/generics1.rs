0cfca8edfcdfed490a74a260f508c763bb7ab9e6
idan.liberman <idanlib@gmail.com>
2023-06-15 23:19:19 +0300

    Rustlings 70/95

--- exercises/generics/generics1.rs
+++ exercises/generics/generics1.rs
@@ -4,7 +4,7 @@
 // Execute `rustlings hint generics1` or use the `hint` watch subcommand for a hint.
 
 fn main() {
-    // let mut shopping_list: Vec<&str> = Vec::new(); //specific type
+    // let mut shopping_list: Vec<&str> = Vec::new(); //annotating the type explicitly 
     let mut shopping_list = Vec::new(); //letting type be dynamically inferred (relies on generics)
     shopping_list.push("milk");
 }
--- exercises/lifetimes/lifetimes3.rs
+++ exercises/lifetimes/lifetimes3.rs
@@ -4,16 +4,18 @@
 //
 // Execute `rustlings hint lifetimes3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
-struct Book {
-    author: &str,
-    title: &str,
+//Add lifetime definition to struct definition, add annotations to struct fields
+struct Book<'a> {
+    author: &'a str,
+    title: &'a str,
 }
 
 fn main() {
     let name = String::from("Jill Smith");
-    let title = String::from("Fish Flying");
+    
+        let title = String::from("Fish Flying");
+    
+    
     let book = Book { author: &name, title: &title };
 
     println!("{} by {}", book.title, book.author);
